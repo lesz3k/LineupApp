9a44cc658345d2b41cb04aed10abe90889a3ea53
leszkasp <leszek.kasperczyk@pearson.com>
2018-04-26 22:44:55 +0200

    minor changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import LineUp from './json/lineup.json';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('null', () => {
+  const n = LineUp;
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).toBeTruthy();
+  expect(n).not.toBeFalsy();
 });
